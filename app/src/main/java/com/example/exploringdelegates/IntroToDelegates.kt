1e21e176f94862492b4bbdeab9195fbc27596905
Meera Vasudevan <meera.vasudevan@rea-group.com>
2019-05-09 12:58:55 +1000

    Add tester

--- app/src/main/java/com/example/exploringdelegates/IntroToDelegates.kt
+++ app/src/main/java/com/example/exploringdelegates/IntroToDelegates.kt
@@ -3,10 +3,12 @@ package com.example.exploringdelegates
 fun main() {
     val alice = Coders()
     val ken = Designers()
-    val tiberius = SuperEmployee(alice, ken)
+    val olive = Testers()
+    val tiberius = SuperEmployee(alice, ken, olive)
 
     tiberius.code()
     tiberius.design()
+    tiberius.test()
 }
 
 //    Consider a Super Employee
@@ -14,7 +16,8 @@ fun main() {
 
 // SuperEmployee implements both coding and designing
 // however it delegates these implementations to other employees using `by`
-internal class SuperEmployee(coder: WhoCanCode, designer: WhoCanDesign) : WhoCanCode by coder, WhoCanDesign by designer
+internal class SuperEmployee(coder: WhoCanCode, designer: WhoCanDesign, tester: WhoCanTest) :
+    WhoCanCode by coder, WhoCanDesign by designer, WhoCanTest by tester
 
 //    Skill sets
 internal interface WhoCanCode {
@@ -23,6 +26,10 @@ internal interface WhoCanCode {
 
 internal interface WhoCanDesign {
     fun design()
+}
+
+internal interface WhoCanTest {
+    fun test()
 }
 
 //    Employees with different skills
@@ -38,3 +45,8 @@ internal class Designers : WhoCanDesign {
     }
 }
 
+internal class Testers: WhoCanTest {
+    override fun test() {
+        println("Employee who can test stuff")
+    }
+}
